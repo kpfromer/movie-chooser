611fea28915c081c6a51f8e86fa98f9f50b979f7
Kyle <kpfromer2@gmail.com>
2019-09-06 15:16:43 -0600

    Add material design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,131 @@
 import React, { useState, useEffect, FormEvent } from "react";
 import "./App.css";
 import axios from "axios";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Link from "@material-ui/core/Link";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import DeleteIcon from "@material-ui/icons/Delete";
+import clsx from "clsx";
+import {
+  ListItemText,
+  ListItem,
+  Input,
+  Button,
+  TextField,
+  ListItemIcon,
+  ListItemSecondaryAction
+} from "@material-ui/core";
 
 const api = !!process.env.REACT_APP_API_URL
   ? process.env.REACT_APP_API_URL
-  : "http://localhost:3000";
+  : "http://localhost:3001";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  fixedHeight: {
+    height: 240
+  }
+}));
 
 const App: React.FC = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
   const [movies, setMovies] = useState<null | { _id: string; name: string }[]>(
     null
   );
@@ -41,7 +160,7 @@ const App: React.FC = () => {
   };
 
   const getRandom = () => {
-    if (movies !== null) {
+    if (movies !== null && movies.length > 0) {
       const index = Math.floor(Math.random() * movies.length);
       setRandomMovie(movies[index]);
     }
@@ -49,27 +168,81 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <h1>Movies:</h1>
-      <ul>
-        {movies === null ? (
-          <li>None</li>
-        ) : (
-          movies.map(movie => (
-            <li key={movie._id} onClick={remove(movie._id)}>
-              {movie.name}
-            </li>
-          ))
-        )}
-      </ul>
-      <form onSubmit={submit}>
-        New Movie:{" "}
-        <input value={movie} onChange={event => setMovie(event.target.value)} />
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={getRandom}>Get Random Movie</button>
-      <h1>
-        Random Movie: "{randomMovie === null ? "none" : randomMovie.name}"
-      </h1>
+      <CssBaseline />
+      <AppBar position="absolute">
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            Movie Chooser
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <h1>Movie List:</h1>
+          <List>
+            {movies === null ? (
+              <li>None</li>
+            ) : (
+              movies.map(movie => (
+                <ListItem key={movie._id}>
+                  <ListItemText>{movie.name}</ListItemText>
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" onClick={remove(movie._id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))
+            )}
+          </List>
+          <form onSubmit={submit}>
+            <TextField
+              value={movie}
+              label="New Movie"
+              onChange={event => setMovie(event.target.value)}
+            />
+            <br />
+            <Button type="submit">Submit</Button>
+          </form>
+          <hr />
+          {movies !== null && movies.length > 0 && (
+            <>
+              <Button onClick={getRandom}>Get Random Movie</Button>
+              {randomMovie !== null && (
+                <>
+                  <Button onClick={remove(randomMovie._id)}>
+                    Remove Random
+                  </Button>
+                  <br />
+                  <Typography variant="h5">
+                    Random Movie: "
+                    {randomMovie === null ? "none" : randomMovie.name}"
+                  </Typography>
+                </>
+              )}
+            </>
+          )}
+        </Container>
+      </main>
     </div>
   );
 };
