5947b6cec54f7ec0a13b694af901d30057126da1
SaurabhTotey <SaurabhTotey@gmail.com>
2019-11-10 21:26:17 -0700

    Added algorithm to choose movies based on their weights

--- src/resolvers/movies.ts
+++ src/resolvers/movies.ts
@@ -30,6 +30,7 @@ export const GET_MOVIES = gql`
       voteAverage
       runtime
       posterPath
+      weight
       tags {
         id
         name
@@ -50,6 +51,7 @@ export const GET_USER_AND_MOVIES_AND_TAGS = gql`
       voteAverage
       runtime
       posterPath
+      weight
       tags {
         id
         name
@@ -75,6 +77,7 @@ export const GET_USER_AND_MOVIES_AND_TAGS = gql`
         voteAverage
         runtime
         posterPath
+        weight
         tags {
           id
           name
@@ -93,6 +96,7 @@ export const GET_MOVIE = gql`
       voteAverage
       runtime
       posterPath
+      weight
       tags {
         id
         name
